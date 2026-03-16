edc3d1314665c41e33a88325a59ef7fd135033fe
채완호 <127wanho@naver.com>
2026-03-16 15:47:36 +0900

    chore: Spring Build

---
+++ src/main/java/com/objection/ObjectionApplication.java
@@ -0,0 +1,13 @@
+package com.objection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectionApplication.class, args);
+    }
+
+}
