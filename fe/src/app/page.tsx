b77be58646b3aea6345d5929fdd03445f51aecb7
허성지 <co4331@naver.com>
2026-03-18 10:31:02 +0900

    feat : ModalFrame 컴포넌트 구현

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
-
+'use client';
+import Image from 'next/image';
+import ModalFrame from '@/components/ui/ModalFrame';
+import { useState } from 'react';
 export default function Home() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Image src="/file.svg" alt="file" width={100} height={100} />
-      Hello world
-      안녕하세요
+      <div className="flex flex-col items-center">
+        Hello world 안녕하세요
+        <button className="border px-2" onClick={() => setOpen(true)}>
+          열기
+        </button>
+      </div>
+      {open && <ModalFrame onClose={() => setOpen(false)}> 멀바 </ModalFrame>}
     </div>
   );
 }
