267da6d93f8d8e58dfa6d815bddfba97fe269118
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-03-18 14:34:19 +0900

    feat/user entity 작성

---
+++ be/src/main/java/com/objection/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.objection.domain.user.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userNo;
+
+    @Column(nullable = false, unique = true,  length = 15)
+    private String userId;
+
+    @Column(nullable = false, length = 60)
+    private String userPw;
+
+    @Column(nullable = false, length = 20)
+    private String userName;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
+
+    public boolean isDeleted() {
+        return this.deletedAt != null;
+    }
+
+}
