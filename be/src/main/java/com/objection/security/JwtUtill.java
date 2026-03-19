fa5cc71be7b2b2de9f7d794448faae5721fcc6b6
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-03-19 09:26:49 +0900

    "feat: JwtUtill getSigningKey 구현"

---
+++ be/src/main/java/com/objection/security/JwtUtill.java
@@ -0,0 +1,25 @@
+package com.objection.security;
+
+
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtill {
+
+    private final JwtProperties jwtProperties;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+
+
+}
