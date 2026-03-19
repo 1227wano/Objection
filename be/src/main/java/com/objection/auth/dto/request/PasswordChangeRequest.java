7d8607b8346dce6ffe8c20f7eca682f995750537
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-03-19 16:06:41 +0900

    "feat: PasswordChangeRequest DTO 구현"

---
+++ be/src/main/java/com/objection/auth/dto/request/PasswordChangeRequest.java
@@ -0,0 +1,16 @@
+package com.objection.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PasswordChangeRequest (
+
+    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
+    String currentPw,
+
+
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 8자 이상 입력해주세요.")
+    String newPw
+
+    ) {}
