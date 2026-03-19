7ce807d9cf25187787adfb88def6b6c90f6921ed
DESKTOP-6OO0P6F\SSAFY <kimminjae2437>
2026-03-19 09:28:28 +0900

    "feat: JwtUtill generateToken 구현"

--- be/src/main/java/com/objection/security/JwtUtill.java
+++ be/src/main/java/com/objection/security/JwtUtill.java
@@ -1,12 +1,14 @@
 package com.objection.security;
 
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -18,6 +20,15 @@ public class JwtUtill {
         return Keys.hmacShaKeyFor(
                 jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8)
         );
+    }
+
+    String generateToken(String userId) {
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtProperties.getExpiration()))
+                .signWith(getSigningKey())
+                .compact();
     }
 
 
