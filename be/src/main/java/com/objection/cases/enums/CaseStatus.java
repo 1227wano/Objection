cad03402c6206f9e30a6c40582bd9cee30cfcb4f
kim <rlaalswo2437@gmail.com>
2026-03-22 17:33:25 +0900

    feat: 사건 Enum 클래스 생성 (CaseStatus, StayStatus, ClaimType)

---
+++ be/src/main/java/com/objection/cases/enums/CaseStatus.java
@@ -0,0 +1,11 @@
+package com.objection.cases.enums;
+
+public enum CaseStatus {
+    STARTED,
+    DOC_UPLOADED,
+    ANALYZING,
+    ANALYSIS_DONE,
+    DOC_GENERATED,
+    SUBMITTED,
+    COMPLETED
+}
