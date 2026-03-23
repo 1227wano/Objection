127b3e215cd26ab0fef61b732d35b000a7ee78d1
허성지 <co4331@naver.com>
2026-03-24 01:05:09 +0900

    fix: rightsidebarFrame 최대 높이 수정

--- fe/src/components/layout/RightSidebarFrame.tsx
+++ fe/src/components/layout/RightSidebarFrame.tsx
@@ -10,7 +10,7 @@ export default function RightSidebarFrame({ children, className }: RightSidebarF
   return (
     <div
       className={cn(
-        'w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5 sticky top-0 self-start h-screen overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]',
+        'w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5 sticky top-0 self-start h-[calc(100vh-3.5rem)] overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]',
         className,
       )}
     >
