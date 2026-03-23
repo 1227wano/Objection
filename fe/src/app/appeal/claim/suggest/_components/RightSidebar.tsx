1e085db64d0b81e4af56b7659c6b0c9b9497b65d
허성지 <co4331@naver.com>
2026-03-23 16:53:49 +0900

    fix: RightSideBar 스크롤 제거

--- fe/src/app/appeal/claim/suggest/_components/RightSidebar.tsx
+++ fe/src/app/appeal/claim/suggest/_components/RightSidebar.tsx
@@ -8,7 +8,7 @@ interface RightSidebarProps {
 
 export default function RightSidebar({ data }: RightSidebarProps) {
   return (
-    <div className="w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5">
+    <div className="w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5 sticky top-0 self-start h-screen overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
       <AISummaryCard data={data} />
       <CaseAccordion precedent={data.representativePrecedent} />
     </div>
--- fe/src/app/appeal/claim/write/_components/RightSidebar.tsx
+++ fe/src/app/appeal/claim/write/_components/RightSidebar.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default function RightSidebar({ legalIssues }: Props) {
   return (
-    <div className="w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5">
+    <div className="w-80 shrink-0 border-l border-gray-200 bg-[#f8fafc] flex flex-col gap-4 p-5 sticky top-0 self-start h-screen overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
       {/* 체크리스트 */}
       <ChecklistCard />
 
